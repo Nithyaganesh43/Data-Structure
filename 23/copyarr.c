680218d90c1baecbe66ef7feb79ac4ce6f6e9ddf
Nithya Ganesh 43 <nithyaganesh4343@gmail.com>
2024-02-24 14:19:31 +0530

    e

---
+++ 23/copyarr.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter the no of array:");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        printf("\nEnter the %d element:",i+1);
+        scanf("%d",&arr[i]);
+    }
+    int narr[n];
+    for(int i=0;i<n;i++){
+    narr[i]=arr[i];
+    }printf("\n the elements new array:");
+     for(int i=0;i<n;i++){
+        printf("%d",narr[i]);
+        
+    }
+}  
